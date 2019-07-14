49d74a4b7512fae6e85347bb21cc9275cd3289eb
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-07-14 21:41:56 +0900

    add: regidentsのリソースにcardsを追加

--- app/Http/Resources/ResidentResource.php
+++ app/Http/Resources/ResidentResource.php
@@ -14,6 +14,12 @@ class ResidentResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'tweetBodies' => CardResource::collection($this->cards),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
